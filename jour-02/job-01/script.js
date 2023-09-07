4967e7d1da7af2cfc2fd2d83b4951b0c33e9f4e5
anais-labit <anais.labit@gmail.com>
2023-09-07 15:23:02 +0200

    job01 >

--- jour-02/job-01/script.js
+++ jour-02/job-01/script.js
@@ -5,7 +5,7 @@ function myChangeBackgroundColor() {
     page.style.backgroundColor = "#ffb703";
   } else if (window.screen.width < 1337 && window.screen.width >= 505) {
     page.style.backgroundColor = "#d90429";
-  } else if (window.screen.width >= 504) {
+  } else if (window.screen.width <= 504) {
     page.style.backgroundColor = "#003049";
   }
 }
